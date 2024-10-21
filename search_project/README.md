d2b3925a13afd65f54001b11f28478b5e4b40026
Justice Sunday <onyekaozuru122@gmail.com>
2024-10-21 17:07:06 +0100

    Search Project

---
+++ search_project/README.md
@@ -0,0 +1,5 @@
+## Author: Sunday Justice Gabriel
+## Project: search project
+## About:
+  * The project search for all related text inputed  at the input field
+  * The project is built using html CSS and JavaScript
---
+++ search_project/script.js
@@ -0,0 +1,14 @@
+const p  =  document.getElementById("p");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", ()=>{
+    let input =  document.getElementById('input').value;
+    
+    if (input !== ""){
+        let regExp = new RegExp(input, 'gi')
+        p.innerHTML =  p.textContent.replace(regExp, "<mark>$&</mark>")
+    }
+
+   
+    document.getElementById("input").value = "";
+})
